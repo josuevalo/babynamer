52366892c1122a0aee3e173023218dbf0af40714
Josue Arevalo <arevalo.josued@gmail.com>
2022-03-04 15:49:36 -0800

    Set up React Router Dom and setup Home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 // import Cookies from 'universal-cookie';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import axios from 'axios'
-import Navbar from './components/Navbar';
+import axios from "axios";
+import Navbar from "./components/Navbar";
+import Home from './Home'
 
 // const cookies = new Cookies();
 
 export default function App() {
-const { state, setState } = useState('')
+  const { state, setState } = useState("");
 
-
-  return ( 
- <div className='App'>
-   <Navbar />
-   Babynamer
- </div>
-
-  )};
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        Babynamer
+          <Link to="/">Home</Link> | <Link to="/expenses">Expenses</Link>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
---
+++ client/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+
+export default function Home() {
+
+  return (
+    <main className="home">
+      <h2>
+        Welcome to babynamer!
+      </h2>
+    </main>
+  );
+}
