e7228d8146cd1c39ab1a95a1ba81843cf5bcc7e9
Josue Arevalo <arevalo.josued@gmail.com>
2022-03-06 20:28:27 -0800

    Connect psql and setup db:reset

--- server/db/seeds/03_voters.sql
+++ server/db/seeds/03_voters.sql
@@ -1,6 +1,6 @@
 INSERT INTO voters (name, email)
-VALUES ('Justin, lala@lala.com'),
-('Alexa, boo@boo.com'),
-('Francis, ahh@ahh.com'),
-('Barb, hi@hi.com'),
-('Luke, luke@luke.com')
+VALUES ('Justin', 'lala@lala.com'),
+('Alexa', 'boo@boo.com'),
+('Francis', 'ahh@ahh.com'),
+('Barb', 'hi@hi.com'),
+('Luke', 'luke@luke.com')
