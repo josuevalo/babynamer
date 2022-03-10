88be488a0c021d04d4f473602bc5ddf166af2db6
Josue Arevalo <arevalo.josued@gmail.com>
2022-03-09 16:21:50 -0800

    Seperate up/down vote into own component

--- client/src/BabyName.jsx
+++ client/src/BabyName.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import Votes from "./components/Votes";
 import "./index.css";
 import { useState, useEffect } from "react";
 
@@ -8,12 +9,6 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Divider from "@mui/material/Divider";
-import Fab from '@mui/material/Fab';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
-import ThumbDownAltIcon from '@mui/icons-material/ThumbDownAlt';
-import { pink } from '@mui/material/colors';
-
 
 export default function BabyName() {
   const [state, setState] = useState({ suggestions: [] });
@@ -33,32 +28,26 @@ export default function BabyName() {
 
   const nameSuggestion = state.suggestions.map((d) => {
     return (
-    <>
-      <ListItem>
-          <ListItemText primary={d.name}  />
-          <Fab size="small" color="primary" aria-label="like">
-      <ThumbUpAltIcon />
-      </Fab>
-      <Fab size="small" aria-label="like">
-      <ThumbDownAltIcon sx={{ color: pink[500] }} />
-      </Fab>
-      </ListItem>
-      <Divider />
-    </>
-   ) });
+      <>
+        <ListItem>
+          <ListItemText primary={d.name} />
+          <Votes />
+        </ListItem>
+        <Divider />
+      </>
+    );
+  });
 
   return (
     <main className="babynames">
       <h2>
-        These are the name suggestions for {state.suggestions[0] && state.suggestions[0].username}:
-        
+        These are the name suggestions for{" "}
+        {state.suggestions[0] && state.suggestions[0].username}:
       </h2>
 
       <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
         <nav aria-label="secondary mailbox folders">
-          <List>
-          {nameSuggestion}
-          </List>
+          <List>{nameSuggestion}</List>
         </nav>
       </Box>
     </main>
---
+++ client/src/components/Votes.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../index.css";
+import { useState } from "react";
+import Fab from "@mui/material/Fab";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import ThumbDownAltIcon from "@mui/icons-material/ThumbDownAlt";
+import { pink } from "@mui/material/colors";
+import Badge from "@mui/material/Badge";
+
+export default function Votes() {
+
+  const [incCount, setIncCount] = useState(0);
+  const incNum = () => {
+    setIncCount(incCount + 1);
+  };
+  const [decCount, setDecCount] = useState(0);
+  const decNum = () => {
+  setDecCount(decCount + 1);
+    };
+ 
+
+
+  return (
+    <main className="votes">
+          <Badge className="badgeNum" badgeContent={incCount} color="primary">
+            <Fab onClick={incNum} size="small"  aria-label="like">
+              <ThumbUpAltIcon color="primary" />
+            </Fab>
+          </Badge>
+          <Badge className="badgeNum" badgeContent={decCount} color="primary" sx={{ color: pink[500] }}>
+            <Fab onClick={decNum} size="small" aria-label="like">
+              <ThumbDownAltIcon sx={{ color: pink[500] }} />
+            </Fab>
+          </Badge>
+    </main>
+  );
+}
