80daf6e23572b2cefe6017ce08cf57958351a599
Josue Arevalo <arevalo.josued@gmail.com>
2022-03-11 18:03:51 -0800

    Add check auth to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,95 @@
-// import Cookies from 'universal-cookie';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./Home";
 import BabyName from "./BabyName";
 
-// const cookies = new Cookies();
+toast.configure();
 
 export default function App() {
+
+  const checkAuthenticated = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/authentication/verify", {
+        method: "POST",
+        headers: { jwt_token: localStorage.token }
+      });
+
+      const parseRes = await res.json();
+
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = boolean => {
+    setIsAuthenticated(boolean);
+  };
+
 
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
         <Routes>
+
+
+
+        <Switch>
+            <Route
+              exact
+              path="/login"
+              render={props =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={props =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={props =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+          </Switch>
+
+
+
+
+
           <Route path="/" element={<Home />} />
           <Route
             path="/:username"
