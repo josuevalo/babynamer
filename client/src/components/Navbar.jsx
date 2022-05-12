577e3aba25cef8439542cba45e4e67984c916958
Josue Arevalo <arevalo.josued@gmail.com>
2022-05-11 21:20:03 -0700

    Implement active links on navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -28,11 +29,14 @@ export default function Navbar(props) {
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
+    navigate(`/`);
   };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
+
+  const navigate = useNavigate();
 
   return (
     <AppBar position="static">
