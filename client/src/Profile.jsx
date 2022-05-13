e15b6fdf457a027f3aff20b1eb4c88fcfcdabdd8
Josue Arevalo <arevalo.josued@gmail.com>
2022-05-12 22:49:14 -0700

    Bring in useCopy hook/library so users can copy link to clipboard for easy sharing

--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import "./App.css";
+import Button from "@mui/material/Button";
+import useCopy from "use-copy";
 
 export default function Profile({ setAuth, isAuthenticated }) {
+  const username = "billandjill";
+  // During development, the URL is local host, but will need to be update once URL is hosted //
+  const [copied, copy, setCopied] = useCopy(
+    `http://localhost:3000/${username}`
+  );
+
+  const copyText = () => {
+    copy();
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  };
+
   return (
     <main className="profile">
       <h1>Welcome to your profile!</h1>
+      <Button variant="outlined">Edit Profile</Button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Button variant="outlined">
+        {copied ? "Copied to clipboard!" : <a onClick={copyText}>Copy text</a>}
+      </Button>
+
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
     </main>
   );
 }
