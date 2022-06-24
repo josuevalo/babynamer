31430f36ed41a37f8d83c3e546577ddd739c90e5
Josue Arevalo <arevalo.josued@gmail.com>
2022-06-23 23:36:51 -0700

    Adjust useState for Filteredbysex

--- client/src/components/FilterBySex.jsx
+++ client/src/components/FilterBySex.jsx
@@ -5,17 +5,14 @@ import BoyIcon from '@mui/icons-material/Boy';
 import GirlIcon from '@mui/icons-material/Girl';
 import WcIcon from '@mui/icons-material/Wc';
 
-export default function FilterbySex({setFilteredSex}) {
-  const [value, setValue] = React.useState("All");
+export default function FilterbySex({filteredSex, setFilteredSex}) {
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
     setFilteredSex(newValue);
-    console.log("VALUE ===>", value)
   };
 
   return (
-    <Tabs value={value} onChange={handleChange} aria-label="filter by sex" centered>
+    <Tabs value={filteredSex} onChange={handleChange} aria-label="filter by sex" centered>
       <Tab icon={<BoyIcon />} value="Boy" label="Boy" />
       <Tab icon={<GirlIcon />} value="Girl" label="Girl" />
       <Tab icon={<WcIcon />} value="All" label="All" />
