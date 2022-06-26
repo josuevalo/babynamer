15ecbb6bd3233390a22d257b76ea6c2c682ae218
Josue Arevalo <arevalo.josued@gmail.com>
2022-06-25 17:24:36 -0700

    Update index.js for prod

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const Express = require("express");
 const App = Express();
 const cors = require('cors');
+const BodyParser = require('body-parser');
+const path = require('path');
+const port = process.env.PORT | 5000;
 
+if (process.env.NODE_ENV === "production") {
+  App.use(Express.static(path.resolve(__dirname, '../client/build')));
+} else {
+  App.use(Express.static('public'));
+}
 
 // Middleware //
 
 App.use(cors());
 App.use(Express.json())
+
+// Express Configuration
+App.use(BodyParser.urlencoded({
+  extended: false
+}));
+App.use(BodyParser.json());
 
 // Routes //
 const suggestionsRoutes = require("./routes/suggestions");
@@ -21,7 +35,9 @@ App.use("/api/add-suggestions", addSuggestionsRoutes);
 App.use("/api/votes", votesRoutes);
 App.use("/api/auth", jwtAuthRoutes);
 
-const port = process.env.PORT | 5000;
+App.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 App.listen(port, () => {
   console.log(`Server is listening on port ${port} 🎉 `);
