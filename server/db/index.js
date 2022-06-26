1bb86c4e40455d2955f204e997257654a07530b8
Josue Arevalo <arevalo.josued@gmail.com>
2022-06-25 17:43:02 -0700

    Update dbParams to use connection string

--- server/db/index.js
+++ server/db/index.js
@@ -3,15 +3,38 @@ require('dotenv').config({
     path: '../.env'
   });
 
-const devConfig = {
+let dbParams = {};
+if (process.env.DATABASE_URL) {
+  dbParams.connectionString = process.env.DATABASE_URL;
+  dbParams.ssl = { rejectUnauthorized: false };
+} else {
+  dbParams = {
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
     database: process.env.PG_DATABASE,
     jwtSecret: process.env.JWT_SECRET
+  };
 }
 
-const pool = new Pool(devConfig)
+// const devConfig = {
+//     user: process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+//     host: process.env.PG_HOST,
+//     port: process.env.PG_PORT,
+//     database: process.env.PG_DATABASE,
+//     jwtSecret: process.env.JWT_SECRET
+// }
+
+const pool = new Pool(dbParams);
+
+pool.connect((err,client) => {
+  if (!err) {
+    console.log('DB in db.index.js connected.\nClient => ', client.user);
+  } else {
+    console.log('Error: ', err);
+  }
+});
 
 module.exports = pool;
